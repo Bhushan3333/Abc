aa90660e242bd6efb6ca1fb99a31f9db879a1873
Asus <Asus@DESKTOP-4DAMJNJ>
2020-03-11 10:56:44 +0530

    Check this

--- SpringBootProjectForJoining/src/main/java/com/webcorestone/crud/Test.java
+++ SpringBootProjectForJoining/src/main/java/com/webcorestone/crud/Test.java
@@ -1,9 +1,16 @@
 package com.webcorestone.crud;
 
 public class Test {
+	
+	public static void m1() {
+		System.out.println("HEllo");
+
+	}
 
 	public static void main(String[] args) {
 	System.out.println("hihihi");
+	
+	Test.m1();
 	}
 
 }
